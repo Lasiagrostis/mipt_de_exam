f55ecdc15256dcd2cffa5f2431677ec009c7a1fa
karachun <lasiagrostis@mail.ru>
2025-06-15 16:35:08 +0300

    Обновление README.md и добавление retry

--- dags/load_train_predict_dag.py
+++ dags/load_train_predict_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.utils.trigger_rule import TriggerRule
+from datetime import datetime, timedelta
 import sys
 import os
 
@@ -10,8 +11,14 @@ from load_data import load_breast_cancer_data
 from train_model import train_logistic_regression
 from predict import make_predictions
 
+default_args = {
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+}
+
 with DAG(
     dag_id="load_train_predict_dag",
+    default_args=default_args,
     start_date=datetime(2024, 1, 1),
     schedule_interval="@daily",
     catchup=False,
@@ -25,12 +32,14 @@ with DAG(
 
     train_model = PythonOperator(
         task_id="train_logistic_regression",
-        python_callable=train_logistic_regression
+        python_callable=train_logistic_regression,
+        trigger_rule=TriggerRule.ALL_DONE
     )
     
     predict_data = PythonOperator(
         task_id="make_predictions",
-        python_callable=make_predictions
+        python_callable=make_predictions,
+        trigger_rule=TriggerRule.ALL_DONE
     )
 
 load_data >> train_model >> predict_data
